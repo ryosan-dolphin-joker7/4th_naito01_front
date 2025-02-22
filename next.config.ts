d43695044781310562513d13c57f125e7c84db74
ryosan-7 <30883561+ryoakira12345@users.noreply.github.com>
2025-02-22 18:55:39 +0900

    standaloneを追加

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "standalone",
+  trailingSlash: true, // 追加
 };
 
 export default nextConfig;
